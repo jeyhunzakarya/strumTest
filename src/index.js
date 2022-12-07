71e14747ab4e43ce617894895d9b72ead5bc5874
JeyhunZakaryayev <zakaryayev@kaist.ac.kr>
2022-12-07 15:17:36 +0900

    removed server

--- src/index.js
+++ src/index.js
@@ -1,95 +1,76 @@
 /**
  * The entry point
  */
-import axios from 'axios'
 
+import { allMessages } from '../samples'
 import App from './components/app'
-const baseUrl = 'http://localhost:3000/messages'  //use this for development
-import "regenerator-runtime"
-const getSingle = (id) => {
-  const num = id%6 +1
-  const allData = axios.get(baseUrl+`/${num}`)
-  return allData
-}
 
 let counter = 0
 
-let allMsgs = []
-
-const loadImagesInitial = async()=>{
+const loadImagesInitial = ()=>{
   let list = document.getElementById("myList");
   for (let i = 0 ; i<30; i++){
-    await getSingle(counter)
-        .then(response=>{
-          let msg = response.data
+      const msg = allMessages[counter]
+      let msgContainer = document.createElement("div");
+      msgContainer.id = "img" + (counter)
 
-          let msgContainer = document.createElement("div");
-          msgContainer.id = "img" + (counter)
+      const author = document.createElement("h1")
+      author.style="width: 200px;word-wrap: break-word;text-align : left"
+      const authorNode = document.createTextNode(msg.name);
+      author.append(authorNode)
+      msgContainer.append(author)
+      
+      const txt = document.createElement("p")
+      txt.style="width: 800px;word-wrap: break-word;text-align : left"
+      const textNode = document.createTextNode(msg.content);
+      txt.append(textNode)
+      msgContainer.append(txt)
 
-          const author = document.createElement("h1")
-          author.style="width: 200px;word-wrap: break-word;text-align : left"
-          const authorNode = document.createTextNode(msg.name);
-          author.append(authorNode)
-          msgContainer.append(author)
-          
-          const txt = document.createElement("p")
-          txt.style="width: 800px;word-wrap: break-word;text-align : left"
-          const textNode = document.createTextNode(msg.content);
-          txt.append(textNode)
-          msgContainer.append(txt)
+      const imgWrapper = document.createElement("img")
+      imgWrapper.src = msg.image
+      imgWrapper.style.height = "100px"
+      imgWrapper.style.width = "100px"
+      imgWrapper.style.marginTop = "20px"
+      msgContainer.append(imgWrapper)
 
-          const imgWrapper = document.createElement("img")
-          imgWrapper.src = msg.image
-          imgWrapper.style.height = "100px"
-          imgWrapper.style.width = "100px"
-          imgWrapper.style.marginTop = "20px"
-          msgContainer.append(imgWrapper)
-
-          list.prepend(msgContainer)
-          allMsgs.push(msg)  
-        })
-        .then(_=>list.scrollTop = list.scrollHeight)
-        counter++
+      list.prepend(msgContainer)
+      counter++
+    }
+    list.scrollTop = list.scrollHeight
   }
-}
 
-const loadImages =async ()=>{
+const loadImages = ()=>{
   let list = document.getElementById("myList");
-  const elem = list.childNodes[0] 
+  const elem = list.childNodes[0]
   for (let i = 0 ; i<30; i++){
-    await getSingle(counter)
-        .then(response=>{
-          let msg = response.data
+    const msg = allMessages[counter]
+    let msgContainer = document.createElement("div");
+    msgContainer.id = "img" + (counter)
 
-          let msgContainer = document.createElement("div");
-          msgContainer.id = "img" + (counter)
+    const author = document.createElement("h1")
+    author.style="width: 200px;word-wrap: break-word;text-align : left"
+    const authorNode = document.createTextNode(msg.name);
+    author.append(authorNode)
+    msgContainer.append(author)
+    
+    const txt = document.createElement("p")
+    txt.style="width: 800px;word-wrap: break-word;text-align : left"
+    const textNode = document.createTextNode(msg.content);
+    txt.append(textNode)
+    msgContainer.append(txt)
 
-          const author = document.createElement("h1")
-          author.style="width: 200px;word-wrap: break-word;text-align : left"
-          const authorNode = document.createTextNode(msg.name);
-          author.append(authorNode)
-          msgContainer.append(author)
+    const imgWrapper = document.createElement("img")
+    imgWrapper.src = msg.image
+    imgWrapper.style.height = "100px"
+    imgWrapper.style.width = "100px"
+    imgWrapper.style.marginTop = "20px"
+    msgContainer.append(imgWrapper)
 
-          const txt = document.createElement("p")
-          txt.style="width: 800px;word-wrap: break-word;text-align : left"
-          const textNode = document.createTextNode(msg.content);
-          txt.append(textNode)
-          msgContainer.append(txt)
-
-          const imgWrapper = document.createElement("img")
-          imgWrapper.src = msg.image
-          imgWrapper.style.height = "100px"
-          imgWrapper.style.width = "100px"
-          imgWrapper.style.marginTop = "20px"
-          msgContainer.append(imgWrapper)
-
-          list.prepend(msgContainer)
-          allMsgs.push(msg)  
-
-        })
-  counter++
+    list.prepend(msgContainer)
   }
+  list.scrollTop = list.scrollHeight
   elem.scrollIntoView()
+  counter++
 }
 
 window.addEventListener('load', () => {
